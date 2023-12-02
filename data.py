5dee35bc0a6b7a2ce704fc8686282fc838b0b218
FEROZ KHAN <102370932+Feroz1522@users.noreply.github.com>
2023-12-02 21:24:08 +0530

    quiz_games_oops
    first push

---
+++ data.py
@@ -0,0 +1,41 @@
+question_data = {
+    "response_code": 0,
+    "results": [
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "United States President John F. Kennedy was assassinated during his presidential "
+                     "\nmotorcade in Atlanta, Georgia on November 22nd, 1963.",
+         "correct_answer": "False", "incorrect_answers": ["True"]},
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "Adolf Hitler was a german soldier in World War I.", "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "Thomas Crapper was a plumber who invented the "
+                     "flushing toilet.",
+         "correct_answer": "False", "incorrect_answers": ["True"]},
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "The French Kingdom helped the United States gain their independence over Great Britain "
+                     "\nduring the Revolutionary War.",
+         "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "The United States Department of Homeland Security was formed in response to the "
+                     "\nSeptember 11th attacks.",
+         "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "Former United States Presidents John Adams and Thomas Jefferson died within hours of "
+                     "\neach other.",
+         "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "In World War ll, Great Britain used inflatable tanks on the ports of Great Britain to "
+                     "\ndivert Hitler away from Normandy D-day landing.",
+         "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "The United States of America declared their independence from the British Empire on "
+                     "\nJuly 4th, 1776.",
+         "correct_answer": "True", "incorrect_answers": ["False"]},
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "The United States was a member of the League of Nations.", "correct_answer": "False",
+         "incorrect_answers": ["True"]},
+        {"type": "boolean", "difficulty": "easy", "category": "History",
+         "question": "The Tienanmen Square protests of 1989 were held in "
+                     "\nHong Kong.",
+         "correct_answer": "False", "incorrect_answers": ["True"]}]}
